41d372005f7051369a50ec6547fe3d97b78b5934
Itamar Junior <cdajuniorf@gmail.com>
2025-03-09 20:53:43 -0300

    feat: Criei a criptografia da senha do usuário.

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from 'express';
+import { hash } from "bcrypt"
 import { z } from 'zod';
 
 class UsersController {
-  create(req: Request, res: Response) {
+  async create(req: Request, res: Response) {
     const bodySchema = z.object({
       name: z.string().trim().min(3),
       email: z.string().email(),
       password: z.string().min(6),
     })
 
-    const { name, email, password} = bodySchema.parse(req.body);
+    const { name, email, password } = bodySchema.parse(req.body);
 
-    return res.json({ message: "ok" });      
+    const hashedPassword = await hash(password, 8)
+
+    return res.json({ message: "ok", hashedPassword });      
   }
 }
 
