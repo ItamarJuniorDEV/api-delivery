1f8ce53073b94f23fe9b16bf1f6a0af7ed8286c1
Itamar Junior <cdajuniorf@gmail.com>
2025-03-10 12:51:43 -0300

    feat: Adicionei o nome do cliente na listagem.

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -23,7 +23,16 @@ class DeliveriesController {
   }
 
   async index(req: Request, res: Response) {
-    const deliveries = await prisma.delivery.findMany();
+    const deliveries = await prisma.delivery.findMany({
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true
+          }
+        }
+      },
+    });
     
     return res.json(deliveries);
   }
