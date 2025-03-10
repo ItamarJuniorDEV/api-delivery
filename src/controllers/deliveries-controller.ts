07700cf4c965155590ee8e6f3f3d79d73f6fdf3b
Itamar Junior <cdajuniorf@gmail.com>
2025-03-10 11:53:06 -0300

    feat: Criei o cadastro de entrega.

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from 'express';
+import { prisma } from '@/database/prisma';
+import { z } from 'zod';
+import { describe } from 'node:test';
 
 class DeliveriesController {
-  create(req: Request, res: Response) {
-    return res.json({ message: 'Ok' });
+  async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    })
+
+    const { user_id, description } = bodySchema.parse(req.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      }
+    })  
+
+    return res.status(201).json();
   }
 }
 
