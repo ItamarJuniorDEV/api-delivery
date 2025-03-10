c4afb186313ac24b73ed3c48444be01a42f7b3a5
Itamar Junior <cdajuniorf@gmail.com>
2025-03-09 21:36:14 -0300

    feat: Criei a recuperação de usuário.

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,7 +1,31 @@
+import { AppError } from "@/utils/AppError";
 import { Request , Response } from "express";
-
+import { prisma } from "@/database/prisma";
+import { compare } from "bcrypt";
+import { z } from "zod";
 class SessionsController {
-  create(req: Request, res: Response) {
+  async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { email, password } = bodySchema.parse(req.body);
+
+    const user = await prisma.user.findFirst({
+       where: { email },
+      });
+
+    if (!user){
+      throw new AppError("Email ou Senha inválido", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Email ou Senha inválido", 401);
+    }
+
     return res.json({ message: "ok" });
   }
 }
