69bbd7da898f2510bc6f5c2853a239f55d82cf7c
Itamar Junior <cdajuniorf@gmail.com>
2025-03-10 14:29:10 -0300

    feat: Cadastrei o Log de pedidos.

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,8 +1,37 @@
 import { Request, Response } from 'express';
+import { AppError } from '@/utils/AppError';
+import { prisma } from '@/database/prisma';
+import { z } from 'zod';
 
 class DeliveryLogsController {
   async create(req: Request, res: Response) {
-    return res.json({ message: "ok" });
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string()
+    })
+
+    const { delivery_id, description } = bodySchema.parse(req.body);
+    
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id }
+    })
+
+    if(!delivery){
+      throw new AppError("Entrega não encontrada",404)
+    }
+
+    if(delivery.status === "processing"){
+      throw new AppError("Entrega em processamento", 404)
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description
+      }
+    })
+    
+    return res.status(201).json()
   }
 }
 
